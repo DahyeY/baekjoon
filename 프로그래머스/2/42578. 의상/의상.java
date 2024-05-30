1872ae83539486d9784d0f54e539227df28d372b
Dahye Yun <76766196+DahyeY@users.noreply.github.com>
2024-05-30 17:00:47 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            if (map.containsKey(cloth[1])) {
+                map.put(cloth[1], map.get(cloth[1]) + 1);
+            } else {
+                map.put(cloth[1], 1);
+            }
+        }
+
+        for (Integer i : map.values()) {
+            answer *= (i + 1);
+        }
+        answer--;
+        return answer;
+    }
+}
