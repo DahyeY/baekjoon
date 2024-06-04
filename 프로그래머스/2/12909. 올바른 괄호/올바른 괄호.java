8ffab533294c9a444bf5715f437aabb175f0f747
Dahye Yun <76766196+DahyeY@users.noreply.github.com>
2024-06-04 15:48:47 +0900

    [level 2] Title: 올바른 괄호, Time: 19.75 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Integer> st = new Stack<Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='(') {
+                st.push(1);
+            } else {
+                if (st.isEmpty()) return false;
+                else
+                    st.pop();
+            }
+        }
+        if (!st.isEmpty()) return false;
+
+        return answer;
+    }
+}
