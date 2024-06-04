0f457862fae26889497e24c94336c9eb312d2eea
Dahye Yun <76766196+DahyeY@users.noreply.github.com>
2024-06-04 15:04:15 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,34 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> q = new LinkedList<Integer>();
         Queue<Integer> workDays = new LinkedList<Integer>();
         List<Integer> list = new ArrayList<Integer>();
 
-        for(int i = 0; i<progresses.length; i++){
-            workDays.add((int)Math.ceil((100-progresses[i])/(double)speeds[i]));
+        for (int i = 0; i < progresses.length; i++) {
+            workDays.add((int) Math.ceil((100 - progresses[i]) / (double) speeds[i]));
         }
-        
-        while(workDays.peek() != null) {
+
+        while (workDays.peek() != null) {
             int cnt = 1;
             int n = workDays.peek();
             workDays.remove();
-            
-            while (workDays.peek() != null && workDays.peek() <= n) {
+
+            while (!workDays.isEmpty() && workDays.peek() <= n) {
                 cnt++;
                 workDays.remove();
             }
             list.add(cnt);
         }
-        
-        System.out.println(list);
 
-        
+
         int[] answer = new int[list.size()];
-        for(int i = 0; i<answer.length; i++){
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
         return answer;
     }
 }
-
