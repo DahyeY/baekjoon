0d1b4f8d2f02e56722317b80b0f177b37feb6dad
Dahye Yun <76766196+DahyeY@users.noreply.github.com>
2026-04-23 19:36:13 +0900

    [level 3] Title: 입국심사, Time: 50.23 ms, Memory: 6.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long left = 1;
+    long long right = (long long)*max_element(times.begin(), times.end())*n;
+    long long answer = right;
+    
+    while(left <= right){
+        long long mid = (left + right) /2;
+        
+        long long total = 0;
+        for (int time : times){
+            total += mid / time;
+        }
+        if (total >= n){
+            right = mid - 1;
+            answer = mid;
+        }else {
+            left = mid + 1;
+        }
+    }
+    
+    return answer;
+}
