1d8a0e8d3c3857bfff9a59132bc8d78b2686fe01
Dahye Yun <76766196+DahyeY@users.noreply.github.com>
2026-05-03 16:18:14 +0900

    [level 3] Title: 단속카메라, Time: 3.80 ms, Memory: 3.98 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(vector<int> a, vector<int> b){
+    return a[1]<b[1];
+}
+
+int solution(vector<vector<int>> routes) {
+    int answer = 0;
+    // 나간 지점 기준으로 정렬.
+    // 카메라 설치 후 걸리는 애들 넘어감.
+    // 카메라 벗어나면 다음 요소 종점으로 카메라 위치 변경
+    
+    sort(routes.begin(), routes.end(), cmp);
+    int camera = -3001;
+    vector<int> visit(routes.size(), 0);
+    
+    for(int i=0; i<routes.size(); i++){
+        if(visit[i]) continue;
+        if(camera >= routes[i][0] && camera <= routes[i][1]) {
+            visit[i] = 1; 
+        }else{
+            camera = routes[i][1];
+            answer++;
+        }
+        
+    }
+    
+    
+    return answer;
+}
